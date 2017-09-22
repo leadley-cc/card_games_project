e1381cea8c3abceef295ef163fed97846afea143
Michael Leadley <leadley75@gmail.com>
2017-09-22 14:22:14 +0100

    Implement a hand for Blackjack/21

---
+++ app/src/main/java/com/example/michael/cardgameproject/twentyone/TwentyOneHand.java
@@ -0,0 +1,53 @@
+package com.example.michael.cardgameproject.twentyone;
+
+import com.example.michael.cardgameproject.playingcards.Card;
+import com.example.michael.cardgameproject.playingcards.CardRank;
+
+import java.util.*;
+
+/**
+ * Created by michael on 22/09/2017.
+ */
+
+class TwentyOneHand {
+    private List<Card> cards;
+
+    public TwentyOneHand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public int getCurrentValue() {
+        int totalValue = 0;
+        int aces = 0;
+        for (Card card : cards) {
+            totalValue += cardValue(card);
+            if (card.getRank() == CardRank.ACE) aces++;
+        }
+        while (totalValue > 21 && aces != 0) {
+            totalValue -= 10;
+            aces--;
+        }
+        return totalValue;
+    }
+
+    public int cardValue(Card card) {
+        switch (card.getRank()) {
+            case ACE:
+                return 11;
+            case JACK:
+            case QUEEN:
+            case KING:
+                return 10;
+            default:
+                return card.getRank().ordinal() + 1;
+        }
+    }
+}
---
+++ app/src/test/java/com/example/michael/cardgameproject/twentyone/TwentyOneHandTest.java
@@ -0,0 +1,76 @@
+package com.example.michael.cardgameproject.twentyone;
+
+import com.example.michael.cardgameproject.playingcards.Card;
+import com.example.michael.cardgameproject.playingcards.CardRank;
+import com.example.michael.cardgameproject.playingcards.CardSuit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by michael on 22/09/2017.
+ */
+public class TwentyOneHandTest {
+    Card card1;
+    Card card2;
+    TwentyOneHand hand;
+
+    @Before
+    public void before() {
+        card1 = new Card(CardRank.ACE, CardSuit.HEARTS);
+        card2 = new Card(CardRank.NINE, CardSuit.CLUBS);
+        hand = new TwentyOneHand();
+    }
+
+    @Test
+    public void getCards() {
+        assertNotNull(hand.getCards());
+    }
+
+    @Test
+    public void addCards() {
+        hand.addCard(card1);
+        hand.addCard(card2);
+        assertEquals(2, hand.getCards().size());
+    }
+
+    @Test
+    public void currentValueStartsAtZero() {
+        assertEquals(0, hand.getCurrentValue());
+    }
+
+    @Test
+    public void aceNineValueIs20() {
+        hand.addCard(card1);
+        hand.addCard(card2);
+        assertEquals(20, hand.getCurrentValue());
+    }
+
+    @Test
+    public void aceNineAceValueIs21() {
+        hand.addCard(card1);
+        hand.addCard(card2);
+        hand.addCard(card1);
+        assertEquals(21, hand.getCurrentValue());
+    }
+
+    @Test
+    public void nineAndManyAcesMakesAllAcesHard() {
+        hand.addCard(card1);
+        hand.addCard(card1);
+        hand.addCard(card2);
+        hand.addCard(card1);
+        hand.addCard(card1);
+        hand.addCard(card1);
+        assertEquals(14, hand.getCurrentValue());
+    }
+
+    @Test
+    public void cardValue() {
+        assertEquals(11, hand.cardValue(card1));
+        assertEquals(9, hand.cardValue(card2));
+    }
+
+}
