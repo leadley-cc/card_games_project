01436c6f46999313578d4e5d25de18d1ce0528cf
Michael Leadley <leadley75@gmail.com>
2017-09-22 14:20:01 +0100

    Add CardRank enum

---
+++ app/src/main/java/com/example/michael/cardgameproject/playingcards/CardRank.java
@@ -0,0 +1,21 @@
+package com.example.michael.cardgameproject.playingcards;
+
+/**
+ * Created by michael on 22/09/2017.
+ */
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
