d3d95b6a9a995e11b13db24b9f374374427ef4e1
Michael Leadley <leadley75@gmail.com>
2017-09-22 14:19:39 +0100

    Add CardSuit enum

---
+++ app/src/main/java/com/example/michael/cardgameproject/playingcards/CardSuit.java
@@ -0,0 +1,12 @@
+package com.example.michael.cardgameproject.playingcards;
+
+/**
+ * Created by michael on 22/09/2017.
+ */
+
+public enum CardSuit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
