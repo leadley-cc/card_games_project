8fffd4428dd32b702b591ac6ebb0ec8c874f9355
Michael Leadley <leadley75@gmail.com>
2017-09-25 19:20:25 +0100

    Add AI for a simple Whist-type game

---
+++ app/src/main/java/com/example/michael/cardgameproject/simplewhist/SimpleWhistAI.java
@@ -0,0 +1,101 @@
+package com.example.michael.cardgameproject.simplewhist;
+
+import com.example.michael.cardgameproject.playingcards.Card;
+import com.example.michael.cardgameproject.playingcards.CardSuit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by michael on 25/09/2017.
+ */
+
+public class SimpleWhistAI {
+    private List<Card> hand;
+
+    public SimpleWhistAI(List<Card> hand) {
+        this.hand = hand;
+    }
+
+    public Card chooseCardFirst() {
+        // Pretty lazy, but for now just play our highest card
+        return highestCard(hand);
+    }
+
+    public Card chooseCardSecond(Card playerCard, CardSuit trumps) {
+        // If we only have one card, play it
+        if (hand.size() == 1) return hand.get(0);
+
+        // Find all the cards we have in the same suit
+        CardSuit currentSuit = playerCard.getSuit();
+        List<Card> cardsInSuit = new ArrayList<>();
+        for (Card card : hand) {
+            if (card.getSuit() == currentSuit) cardsInSuit.add(card);
+        }
+
+        // If we don't have any cards in the same suit we want to trump
+        if (cardsInSuit.size() == 0) {
+            List<Card> cardsInTrumps = new ArrayList<>();
+            for (Card card : hand) {
+                if (card.getSuit() == trumps) cardsInTrumps.add(card);
+            }
+            Card lowestTrump = lowestCard(cardsInTrumps);
+
+            // If we don't have any trumps either, throw away our lowest card
+            if (lowestTrump == null) return lowestCard(hand);
+
+            // Otherwise, return our lowest trump to win with
+            return lowestTrump;
+        }
+
+        // Try to find the lowest card we have that will win
+        Card lowestCardToWin = null;
+        int lowestCardToWinValue = Integer.MAX_VALUE;
+        for (Card card : cardsInSuit) {
+            if ( cardValue(card) > cardValue(playerCard)
+                    && cardValue(card) < lowestCardToWinValue ) {
+                lowestCardToWin = card;
+                lowestCardToWinValue = cardValue(card);
+            }
+        }
+
+        // If we didn't find any winner, play the lowest card we have in the suit
+        if (lowestCardToWin == null) return lowestCard(cardsInSuit);
+
+        // Otherwise return our winner
+        return lowestCardToWin;
+    }
+
+    private Card highestCard(List<Card> cards) {
+        Card highestCard = null;
+        int highestCardValue = Integer.MIN_VALUE;
+        for (Card card : cards) {
+            if (cardValue(card) > highestCardValue) {
+                highestCard = card;
+                highestCardValue = cardValue(card);
+            }
+        }
+        return highestCard;
+    }
+
+    private Card lowestCard(List<Card> cards) {
+        Card lowestCard = null;
+        int lowestCardValue = Integer.MAX_VALUE;
+        for (Card card : cards) {
+            if (cardValue(card) < lowestCardValue) {
+                lowestCard = card;
+                lowestCardValue = cardValue(card);
+            }
+        }
+        return lowestCard;
+    }
+
+    private int cardValue(Card card) {
+        switch (card.getRank()) {
+            case ACE:
+                return 13;
+            default:
+                return card.getRank().ordinal();
+        }
+    }
+}
---
+++ app/src/test/java/com/example/michael/cardgameproject/simplewhist/SimpleWhistAITest.java
@@ -0,0 +1,119 @@
+package com.example.michael.cardgameproject.simplewhist;
+
+import com.example.michael.cardgameproject.playingcards.Card;
+import com.example.michael.cardgameproject.playingcards.CardRank;
+import com.example.michael.cardgameproject.playingcards.CardSuit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by michael on 25/09/2017.
+ */
+
+public class SimpleWhistAITest {
+    private Card twoOfDiamonds;
+    private Card fiveOfClubs;
+    private Card sevenOfSpades;
+    private Card nineOfClubs;
+    private Card jackOfClubs;
+    private Card kingOfSpades;
+    private Card aceOfDiamonds;
+    private SimpleWhistAI testAI;
+    private CardSuit trumps;
+
+    @Before
+    public void before() {
+        twoOfDiamonds = new Card(CardRank.TWO, CardSuit.DIAMONDS);
+        fiveOfClubs = new Card(CardRank.FIVE, CardSuit.CLUBS);
+        sevenOfSpades = new Card(CardRank.SEVEN, CardSuit.SPADES);
+        nineOfClubs = new Card(CardRank.NINE, CardSuit.CLUBS);
+        jackOfClubs = new Card(CardRank.JACK, CardSuit.CLUBS);
+        kingOfSpades = new Card(CardRank.KING, CardSuit.SPADES);
+        aceOfDiamonds = new Card(CardRank.ACE, CardSuit.DIAMONDS);
+        List<Card> aiCards = new ArrayList<>( Arrays.asList(
+                twoOfDiamonds, fiveOfClubs, sevenOfSpades,
+                nineOfClubs, jackOfClubs, kingOfSpades, aceOfDiamonds
+        ));
+        testAI = new SimpleWhistAI(aiCards);
+        trumps = CardSuit.SPADES;
+    }
+
+    @Test
+    public void chooseCardFirst() {
+        assertEquals(aceOfDiamonds, testAI.chooseCardFirst());
+    }
+
+    @Test
+    public void chooseFiveAgainstQueenOfClubs() {
+        Card expected = fiveOfClubs;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.QUEEN, CardSuit.CLUBS), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseSevenOfSpadesAgainstTenOfHearts() {
+        Card expected = sevenOfSpades;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.TEN, CardSuit.HEARTS), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseNineAgainstSevenOfClubs() {
+        Card expected = nineOfClubs;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.SEVEN, CardSuit.CLUBS), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseJackAgainstTenOfClubs() {
+        Card expected = jackOfClubs;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.TEN, CardSuit.CLUBS), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseKingAgainstQueenOfSpades() {
+        Card expected = kingOfSpades;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.QUEEN, CardSuit.SPADES), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseAceToBeatFiveOfDiamonds() {
+        Card expected = aceOfDiamonds;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.FIVE, CardSuit.DIAMONDS), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void chooseTwoOfDiamondsAgainstSixOfSpadesWhenNoTrumps() {
+        List<Card> aiCards = new ArrayList<>( Arrays.asList( twoOfDiamonds, fiveOfClubs ));
+        testAI = new SimpleWhistAI(aiCards);
+
+        Card expected = twoOfDiamonds;
+        Card result = testAI.chooseCardSecond(
+                new Card(CardRank.SIX, CardSuit.SPADES), trumps
+        );
+        assertEquals(expected, result);
+    }
+
+}
