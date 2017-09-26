eccb528e42492a8dc92f18a824c4652a02d2fde1
Michael Leadley <leadley75@gmail.com>
2017-09-26 12:37:10 +0100

    Add Runner for console Whist game

---
+++ app/src/main/java/com/example/michael/cardgameproject/simplewhist/SimpleWhistRunner.java
@@ -0,0 +1,21 @@
+package com.example.michael.cardgameproject.simplewhist;
+
+import com.example.michael.cardgameproject.playingcards.Deck;
+
+/**
+ * Created by michael on 25/09/2017.
+ */
+
+public class SimpleWhistRunner {
+    private static Deck deck;
+    private static SimpleWhistUI gameUI;
+    private static SimpleWhistGame game;
+
+    public static void main(String[] args) {
+        deck = new Deck();
+        gameUI = new SimpleWhistConsole();
+        game = new SimpleWhistGame(deck, gameUI);
+
+        game.playGame();
+    }
+}
