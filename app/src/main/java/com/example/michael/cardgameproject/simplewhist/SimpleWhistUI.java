d59306b0af50f4f95e76a69d9b19ce43e115991c
Michael Leadley <leadley75@gmail.com>
2017-09-26 12:35:26 +0100

    Add UI Interface for Whist game

---
+++ app/src/main/java/com/example/michael/cardgameproject/simplewhist/SimpleWhistUI.java
@@ -0,0 +1,27 @@
+package com.example.michael.cardgameproject.simplewhist;
+
+import com.example.michael.cardgameproject.GameWinner;
+import com.example.michael.cardgameproject.playingcards.Card;
+import com.example.michael.cardgameproject.playingcards.CardSuit;
+
+import java.util.List;
+
+/**
+ * Created by michael on 25/09/2017.
+ */
+
+interface SimpleWhistUI {
+    void showPlayerHand(List<Card> playerHand);
+
+    void showTrumps(CardSuit trumps);
+
+    Card getPlayerCard(List<Card> playerHand);
+
+    void showDealerCard(Card dealerCard);
+
+    void showTrickWinner(GameWinner trickWinner);
+
+    void showScores(int dealerTricks, int playerTricks);
+
+    void showGameWinner(GameWinner gameWinner);
+}
